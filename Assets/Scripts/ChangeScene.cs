4c96424f291753e4ccc717235b0ad802702a5971
marianamaysa <marianamaysaestudos@gmail.com>
2025-03-26 20:07:19 -0300

    Menu - ChangeScene Script
    Menu feito, podendo transicionar de cenas e opção para retornar ao menu

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void Change(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
